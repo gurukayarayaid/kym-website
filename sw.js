d611c02102626ad8f067e7303eddf11407fd0492
WabCraft Studio <guru.kayaraya.id@gmail.com>
2026-09-15 09:42:21 +0700

    chore(pwa): bump cache v43 for landing kirim update

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@
    - Aset (ikon, logo): cache-first
    - Permintaan ke Google Apps Script: TIDAK pernah di-cache
    ============================================================ */
-var CACHE = 'kym-v42';
+var CACHE = 'kym-v43';
 var SHELL = [
   './',
   './index.html',
